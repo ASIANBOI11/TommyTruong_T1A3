23d771a7b7ae1799a4cd2b9dbef7658a0cb5b7c3
TommyTruong <tommy.truong962@gmail.com>
2022-04-03 20:38:07 +1000

    test cases

---
+++ spec/budgeting_spec.rb
@@ -0,0 +1,32 @@
+require './budgeting'
+require 'tty-prompt'
+
+describe Budgeting do
+  describe 'edit budget' do
+    # This checks that the value changing is an appropriate value in this context.
+    # Shows the right value output of changing budget limit.
+    it 'Allows number' do
+      expect('12312').to match(/^[+]?([.]\d+|\d+[.]?\d*)$/)
+    end
+    it 'Does not allow characters' do
+      expect('12asd2').not_to match(/^[+]?([.]\d+|\d+[.]?\d*)$/)
+    end
+    it 'Does not allow negative number' do
+      expect('-999').not_to match(/^[+]?([.]\d+|\d+[.]?\d*)$/)
+    end
+
+    # Shows the right value output of changing the dates
+    it 'Allows only a specific set of character and numbers in order to form dates' do
+      expect('31/12/2003').to match(%r{(?<day>\d{1,2})/(?<month>\d{1,2})/(?<year>\d{4})})
+    end
+    it 'Does not allow input' do
+      expect('312/as/2003').not_to match(%r{(?<day>\d{1,2})/(?<month>\d{1,2})/(?<year>\d{4})})
+    end
+  end
+
+#   describe 'review budget' do
+#     it 'Displays the users budget information' do
+        
+
+#   end
+end
--- spec/edit_budget_spec.rb
+++
@@ -1,1 +0,0 @@
-require '../budgeting.rb'
